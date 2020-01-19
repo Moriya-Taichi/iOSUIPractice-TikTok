d0544d16d91ddf50effbd25fc7ab0b3c06e0d245
Moriya-Taichi <minecraft0125@gmail.com>
2020-01-19 23:11:54 +0900

    implement: VC instantiate logic

--- TikTokUI/Extensions/UIViewController+instantiate.swift
+++ TikTokUI/Extensions/UIViewController+instantiate.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2020 Mori. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum StoryboardName: String {
+    case FeedTab = "FeedTabViewController"
+    case Feed = "FeedViewController"
+}
+
+protocol StoryboardInstantiate {
+    static var storyboardName: StoryboardName { get }
+}
+
+extension StoryboardInstantiate where Self: UIViewController {
+    static func instantiate() -> Self {
+        let storyboard = UIStoryboard(name: storyboardName.rawValue, bundle: nil)
+        let viewControllerINstance = storyboard.instantiateViewController(withIdentifier: String(describing: self)) as! Self
+        return viewControllerINstance
+    }
+}
