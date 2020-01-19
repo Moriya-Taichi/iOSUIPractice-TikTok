c55b8ba9414856c9c48396190ab7aa70b2ca38a1
Moriya-Taichi <minecraft0125@gmail.com>
2020-01-19 16:48:31 +0900

    implement: Add IBOUtlet

--- TikTokUI/UI/Components/TableViewCells/MainFeedMovieCell/MainFeedMovieCell.swift
+++ TikTokUI/UI/Components/TableViewCells/MainFeedMovieCell/MainFeedMovieCell.swift
@@ -11,6 +11,24 @@ import UIKit
 
 final class MainFeedMovieCell: UITableViewCell {
 
+    @IBOutlet weak var movieView: UIImageView!
+
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var userNameScrollLabel: UILabel!
+    
+    @IBOutlet weak var userIconButton: UIButton!
+    @IBOutlet weak var followButton: UIButton!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var commentButton: UIButton!
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var musicButton: UIButton!
+
+
+    @IBOutlet weak var likeCountLabel: UILabel!
+    @IBOutlet weak var commentCountLabel: UILabel!
+    @IBOutlet weak var shareCountLabel: UILabel!
+
     override func awakeFromNib() {
         super.awakeFromNib()
         setupRx()
