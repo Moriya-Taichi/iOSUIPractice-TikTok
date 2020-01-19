082f9ae1da070b821d41d7930254463c36b3f28d
Moriya-Taichi <minecraft0125@gmail.com>
2020-01-19 23:55:03 +0900

    implement: Base Button Action

--- TikTokUI/UI/ViewControllers/Tab/MainTabBarViewController.swift
+++ TikTokUI/UI/ViewControllers/Tab/MainTabBarViewController.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2020 Mori. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
+import UIKit
+
+final class MainTabBarViewController: UITabBarController {
+
+    var callback: Callback?
+    var disposeBag = DisposeBag()
+
+    struct Callback {
+        let showPostContent: (() -> Void)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupRx()
+    }
+
+    private func setupRx() {
+        self.rx.didSelect
+            .map { $0 as? UINavigationController }
+            .filter { $0?.viewControllers.first as? PostContentViewController != nil }
+            .subscribe(onNext: {[weak self] _ in
+                self?.callback?.showPostContent()
+            }).disposed(by: disposeBag)
+    }
+}
