18fc8e48e07b9ddb8d515c0b9b2be5f6fbae4180
Moriya-Taichi <minecraft0125@gmail.com>
2020-01-20 01:41:03 +0900

    implement:Callback and setup reactor

--- TikTokUI/UI/ViewControllers/Feed/FeedViewController.swift
+++ TikTokUI/UI/ViewControllers/Feed/FeedViewController.swift
@@ -6,13 +6,31 @@
 //  Copyright © 2020 Mori. All rights reserved.
 //
 
+import ReactorKit
 import Foundation
+import RxSwift
 import UIKit
 
 final class FeedViewController: UIViewController, StoryboardInstantiate {
+
     static var storyboardName: StoryboardName = .Feed
+    var callback: Callback?
+    var disposeBag = DisposeBag()
+
+    struct Callback {
+        let showUser: ((_ userIdentifier: String) -> Void)
+        let showComment: ((_ postIdentifier: String) -> Void)
+        let showShare: ((_ postIdentifier: String) -> Void)
+        let showSameMusic: ((_ musicIdentifier: String) -> Void)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension FeedViewController: StoryboardView {
+    func  bind(reactor: FeedViewReactor) {
+
     }
 }
