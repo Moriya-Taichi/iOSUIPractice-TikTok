205817af12fa9284205d935d04033c716c6d671c
Moriya-Taichi <minecraft0125@gmail.com>
2020-01-21 23:37:26 +0900

    implement: base layout and setup cell

--- TikTokUI/UI/Components/TableViewCells/TrendFeedCell/TrendFeedCell.swift
+++ TikTokUI/UI/Components/TableViewCells/TrendFeedCell/TrendFeedCell.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2020 Mori. All rights reserved.
 //
 
+import ReactorKit
 import Foundation
+import RxSwift
+import UIKit
+
+final class TrendFeedCell: UITableViewCell {
+
+    @IBOutlet weak var trendMovieCollectionView: UICollectionView!
+    @IBOutlet weak var trendWordLabel: UILabel!
+    @IBOutlet weak var postCountLabel: UILabel!
+
+    var disposeBag = DisposeBag()
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
+
+extension TrendFeedCell: StoryboardView {
+    func bind(reactor: TrendFeedCellReactor) {
+
+    }
+}
