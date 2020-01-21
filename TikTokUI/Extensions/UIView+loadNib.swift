89bddf992ab901128735f5476a4badd7b5568d72
Moriya-Taichi <minecraft0125@gmail.com>
2020-01-22 02:27:45 +0900

    implement: Add SomeNew File, Extension, View, xib

---
+++ TikTokUI/Extensions/UIView+loadNib.swift
@@ -0,0 +1,9 @@
+//
+//  UIView+loadNib.swift
+//  TikTokUI
+//
+//  Created by Mori on 2020/01/22.
+//  Copyright © 2020 Mori. All rights reserved.
+//
+
+import Foundation
---
+++ TikTokUI/UI/Components/CustomViews/SearchHeaderView/SearchHeaderViewReactor.swift
@@ -0,0 +1,9 @@
+//
+//  SearchHeaderViewReactor.swift
+//  TikTokUI
+//
+//  Created by Mori on 2020/01/22.
+//  Copyright © 2020 Mori. All rights reserved.
+//
+
+import Foundation
