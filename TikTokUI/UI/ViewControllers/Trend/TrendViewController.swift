939407734307d605a8cae2964f348b04eb88a07b
Moriya-Taichi <minecraft0125@gmail.com>
2020-01-24 23:37:48 +0900

    implemnt: Setup Trend ViewController

--- TikTokUI/UI/ViewControllers/Trend/TrendViewController.swift
+++ TikTokUI/UI/ViewControllers/Trend/TrendViewController.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2020 Mori. All rights reserved.
 //
 
+import ReactorKit
 import Foundation
+import RxSwift
+import UIKit
+
+final class TrendViewController: UIViewController, StoryboardInstantiate {
+
+    static var storyboardName: StoryboardName = .Trend
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
