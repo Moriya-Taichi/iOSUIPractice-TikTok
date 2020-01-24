e68467ee78d1aafc31b655abce48cec1938e9d3d
Moriya-Taichi <minecraft0125@gmail.com>
2020-01-24 23:33:55 +0900

    implement: Setup User Post ViewController

--- TikTokUI/UI/ViewControllers/User/UserPostVieewController.swift
+++ TikTokUI/UI/ViewControllers/User/UserPostVieewController.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2020 Mori. All rights reserved.
 //
 
+import ReactorKit
 import Foundation
+import RxSwift
+import UIKit
+
+final class UserPostViewController: UIViewController, StoryboardInstantiate {
+
+    static var storyboardName: StoryboardName = .UserPost
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
