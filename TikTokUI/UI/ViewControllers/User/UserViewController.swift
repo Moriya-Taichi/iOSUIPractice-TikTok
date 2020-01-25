139da129d93493578fef2d3992d6e7716846c67b
Moriya-Taichi <minecraft0125@gmail.com>
2020-01-25 23:34:48 +0900

    implement: Add IBOutlet to User View

--- TikTokUI/UI/ViewControllers/User/UserViewController.swift
+++ TikTokUI/UI/ViewControllers/User/UserViewController.swift
@@ -6,13 +6,18 @@
 //  Copyright © 2020 Mori. All rights reserved.
 //
 
+import XLPagerTabStrip
 import Foundation
 import RxSwift
 import UIKit
 
-final class UserViewController: UIViewController, StoryboardInstantiate {
+final class UserViewController: ButtonBarPagerTabStripViewController, StoryboardInstantiate {
 
     static var storyboardName: StoryboardName = .User
+
+    var pages: [UIViewController] = []
+
+    @IBOutlet weak var userHeaderView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
