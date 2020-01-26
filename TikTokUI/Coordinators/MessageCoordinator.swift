aca14811a9d2ab91e81c9f51fb2d9dd04caa90f3
Moriya-Taichi <minecraft0125@gmail.com>
2020-01-26 20:34:23 +0900

    implement: Setup Message Coodinator

--- TikTokUI/Coordinators/MessageCoordinator.swift
+++ TikTokUI/Coordinators/MessageCoordinator.swift
@@ -1,9 +1,33 @@
-g//
-//  Messaf.swift
+//
+//  MessageCoordinator.swift
 //  TikTokUI
 //
 //  Created by Mori on 2020/01/26.
 //  Copyright © 2020 Mori. All rights reserved.
 //
 
-import Foundation
+import SwinjectAutoregistration
+import Swinject
+import UIKit
+
+final class MessageCoodinator: NavigationCoordinator {
+
+    var navigationController: UINavigationController
+    let resolver: Resolver
+
+    init(presenter: UINavigationController, resolver: Resolver) {
+        self.navigationController = presenter
+        self.resolver = resolver
+    }
+
+    func start() {
+        let messageViewContrller = MessageViewController.instantiate()
+        self.navigationController.viewControllers = [messageViewContrller]
+    }
+
+    func navigate(fromLink: Bool) {
+
+    }
+}
+
+
