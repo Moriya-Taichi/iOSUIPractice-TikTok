225bea9d75b5ba432ebc46f26f052793def25889
Moriya-Taichi <minecraft0125@gmail.com>
2020-01-26 20:35:44 +0900

    implement: Setup Feed Coodinator

--- TikTokUI/Coordinators/FeedCoodinator.swift
+++ TikTokUI/Coordinators/FeedCoodinator.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2020 Mori. All rights reserved.
 //
 
-import Foundation
+import SwinjectAutoregistration
+import Swinject
+import UIKit
+
+final class FeedCoodinator: NavigationCoordinator {
+
+    let navigationController: UINavigationController
+    let resolver: Resolver
+
+    init(presenter: UINavigationController, resolver: Resolver) {
+        self.resolver = resolver
+        self.navigationController = presenter
+    }
+
+    func start() {
+        let feedTabViewController = FeedTabViewController.instantiate()
+        let recommendFeedViewController = FeedViewController.instantiate()
+        let followFeedViewController = FeedViewController.instantiate()
+
+        feedTabViewController.pages = [recommendFeedViewController, followFeedViewController]
+        self.navigationController.viewControllers = [feedTabViewController]
+    }
+
+    func navigate(fromLink: Bool) {
+
+    }
+}
