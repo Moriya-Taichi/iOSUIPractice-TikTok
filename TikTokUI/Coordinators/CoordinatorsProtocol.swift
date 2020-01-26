58750328e07c0e5c8be9aefa9acb176734d6b160
Moriya-Taichi <minecraft0125@gmail.com>
2020-01-26 20:39:39 +0900

    implement: Add Coordinator Protcol

--- TikTokUI/Coordinators/CoordinatorsProtocol.swift
+++ TikTokUI/Coordinators/CoordinatorsProtocol.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+import Swinject
+import UIKit
+
+protocol Coordinator: class {
+    var resolver: Resolver { get }
+    func start()
+    func navigate(fromLink: Bool)
+}
+
+protocol ContainerCoordinator: Coordinator {
+    var presenter: UIViewController { get }
+}
+
+protocol NavigationCoordinator: ContainerCoordinator {
+    var navigationController: UINavigationController { get }
+}
+
+extension NavigationCoordinator {
+    var presenter: UIViewController { return navigationController }
+}
+
+protocol TabCoordinator: ContainerCoordinator {
+    var tabBarController: UITabBarController { get }
+}
+
+extension TabCoordinator {
+    var presenter: UIViewController { return tabBarController }
+}
