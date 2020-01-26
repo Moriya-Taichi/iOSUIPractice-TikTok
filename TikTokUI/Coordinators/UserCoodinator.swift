4f0ba8716509e42ec9f27485d66d0f78b5f88cf5
Moriya-Taichi <minecraft0125@gmail.com>
2020-01-26 20:43:18 +0900

    implment: Setup User Coordinator

--- TikTokUI/Coordinators/UserCoodinator.swift
+++ TikTokUI/Coordinators/UserCoodinator.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2020 Mori. All rights reserved.
 //
 
-import Foundation
+import SwinjectAutoregistration
+import Swinject
+import UIKit
+
+final class UserCoodinator: NavigationCoordinator {
+
+    var navigationController: UINavigationController
+    let resolver: Resolver
+
+    init(presenter: UINavigationController, resolver: Resolver) {
+        self.navigationController = presenter
+        self.resolver = resolver
+    }
+
+    func start() {
+        let userTabViewController = UserViewController.instantiate()
+        let userFavoriteViewController = UserFavoriteViewController.instantiate()
+        let userPostViewController = UserPostViewController.instantiate()
+
+        userTabViewController.pages = [userPostViewController, userFavoriteViewController]
+        self.navigationController.viewControllers = [userTabViewController]
+    }
+
+    func navigate(fromLink: Bool) {
+
+    }
+}
